55527837eaba0b6d02409c83fdd67de740a7fa25
Colum Foskin <columfoskin@gmail.com>
2016-10-12 18:37:47 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,83 @@
+// Get the packages we need
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+// Connect to the dvdStore MongoDB
+mongoose.connect('mongodb://localhost:/dvdStoreDb');
+var Dvd = require('./models/dvd');
+
+// Create our Express application
+var app = express();
+// Use the body-parser package in our application
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Use environment defined port or 3000
+var port = process.env.PORT || 3000;
+
+// Create our Express router
+var router = express.Router();
+
+// Initial dummy route for testing
+// http://localhost:3000/api
+router.get('/', function(req, res) {
+    res.json({ message: 'You are running out of dvds!' });
+});
+
+// Register all our routes with /api
+app.use('/api', router);
+
+// Start the server
+app.listen(port);
+console.log('Add dvd on port ' + port);
+
+
+// Create a new route with the prefix /dvds
+var dvdsRoute = router.route('/dvds');
+
+// Create endpoint /api/dvds for POSTS
+dvdsRoute.post(function(req, res) {
+    // Create a new instance of the Dvd model
+    var dvd = new Dvd();
+
+    // Set the dvd properties that came from the POST data
+    dvd.name = req.body.name;
+    dvd.genre = req.body.genre;
+    dvd.certification = req.body.certification;
+    dvd.quantity = req.body.quantity;
+
+    // Save the dvd and check for errors
+    dvd.save(function(err) {
+        if (err)
+            res.send(err);
+
+        res.json({ message: 'Dvd added to the dvdStore!', data: dvd });
+    });
+});
+
+
+// Create endpoint /api/dvds for GET
+dvdsRoute.get(function(req, res) {
+    // Use the Beer model to find all beer
+    Dvd.find(function(err, dvds) {
+        if (err)
+            res.send(err);
+
+        res.json(dvds);
+    });
+});
+
+// Create a new route with the /dvds/:dvd_id prefix
+var dvdRoute = router.route('/beers/:beer_id');
+
+// Create endpoint /api/dvds/:dvd_id for GET
+dvdRoute.get(function(req, res) {
+    // Use the Dvd model to find a specific dvd
+    Dvd.findById(req.params.dvd_id, function(err, dvd) {
+        if (err)
+            res.send(err);
+        res.json(beer);
+    });
+});
---
+++ models/dvd.js
@@ -0,0 +1,13 @@
+// Load required packages
+var mongoose = require('mongoose');
+
+// Define our dvd schema
+var DvdSchema   = new mongoose.Schema({
+  name: String,
+  genre: String,
+  certification: String,
+  quantity: Number
+});
+
+// Export the Mongoose model
+module.exports = mongoose.model('Dvd', DvdSchema);
